0e890ddce6477c862f28490dd7b95b3a13104aac
Kinky Lemons <92684350+redears-lambda@users.noreply.github.com>
2022-01-28 19:47:35 +0800

    add route to landing page

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from distutils.log import debug
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+@app.route("/")
+@app.route("/landing")
+@app.route("/index")
+def render_landing():
+    return render_template("landing.html")
 
 
 if __name__ == "__main__":
