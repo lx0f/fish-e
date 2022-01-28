d505741356400421d564f65336dc8993cda0eb77
Kinky Lemons <92684350+redears-lambda@users.noreply.github.com>
2022-01-28 20:21:17 +0800

    change flask-bootstrap to bootstrap-flask

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 
 
 app = Flask(__name__)
-Bootstrap(app)
+Bootstrap5(app)
 
 @app.route("/")
 def render_landing():
